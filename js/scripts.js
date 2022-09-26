268662360bd7b2d644dc7d48d156daeafb41e39b
Wesley Dionisio <wesley.r.dionisio@gmail.com>
2022-09-26 17:03:14 -0300

    in progress

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,9 @@ $(".bt-menu").on("click", function (e) {
   document.getElementById("hamburguerMenu").classList.toggle("sideMenu");
 });
 
-let tabs = document.getElementById("portfolioTabs");
-let radio1 = document.getElementById("radio-1");
-let radio2 = document.getElementById("radio-2");
+let tabs = document.getElementById("contentSlider");
+let radio1 = document.getElementById("radio1");
+let radio2 = document.getElementById("radio2");
 let content1 = document.getElementById("content1");
 let content2 = document.getElementById("content2");
 
@@ -28,10 +28,13 @@ tabs.addEventListener("change", function () {
   if (radio1.checked) {
     content1.classList.toggle("hidden");
     content2.classList.toggle("hidden");
+    content1.classList.toggle('fadeIn');
+  
     console.log(radio1.checked);
   } else if (radio2.checked) {
     content2.classList.toggle("hidden");
     content1.classList.toggle("hidden");
+    content2.classList.toggle('fadeIn');
     console.log(radio2.checked);
   }
 }, false);
