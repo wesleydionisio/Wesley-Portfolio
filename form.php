ad637d574277e51c3de5f2bdab7fa43eaa1339b6
Wesley Dionisio <wesley.r.dionisio@gmail.com>
2022-09-27 16:55:23 -0300

    testing

--- form.php
+++ form.php
@@ -1,52 +1,29 @@
-<?php 
-$errors = '';
-$myemail = 'contato@wesleydionisio.dev.br';//<-----Put Your email address here.
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
-
-$name = $_POST['name']; 
-$email_address = $_POST['email'];
-$phone = $_POST['phone']; 
-$message = $_POST['message']; 
-
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
-{
-    $errors .= "\n Erro: E-mail inválido.";
-}
-
-if( empty($errors))
-{
-	$to = $myemail; 
-	$email_subject = "Formulário recebido de: $name";
-	$email_body = "Você recebeu uma mensagem a partir de formulário. ".
-	" Aqui está os detalhes:\n Nome: $name \n E-mail: $email_address \n Celular/WhatsApp: $phone \n Mensagem: \n $message"; 
-	
-	$headers = "From: $myemail\n"; 
-	$headers .= "Reply-To: $email_address";
-	
-	mail($to,$email_subject,$email_body,$headers);
-	//redirect to the 'thank you' page
-	header('Location: contact-form-thank-you.html');
-} 
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
-<html>
-<head>
-	<title>Contact form handler</title>
-</head>
-
-<body>
-<!-- This page is displayed only if there is some error -->
 <?php
-echo nl2br($errors);
+if ( ($_POST['name']!="") && ($_POST['email']!="")){
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+$to = "contato@wesleydionisio.dev.br";
+$subject = "AllPHPTricks Contact Form Email";
+$message = "<p>New email is received from $name.</p>
+<p>$message</p><br>WhatsApp: $phone";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= "From: <".$email.">" . "\r\n";
+$sent = mail($to,$subject,$message,$headers);
+if($sent){
+	echo "<span style='color:green; font-weight:bold;'>
+	Thank you for contacting us, we will get back to you shortly.
+	</span>";
+}
+else{
+	echo "<span style='color:red; font-weight:bold;'>
+	Sorry! Your form submission is failed.
+	</span>";
+	}
+}
 ?>
-
-
-</body>
-</html>
